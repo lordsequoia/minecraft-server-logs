e4d6a49be3f3f39ec014a4e51a2c9ae9b07be342
LordSequoia <thelordsequoia@gmail.com>
2022-07-06 14:59:50 +0000

    chore: documentation for utils

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,55 +3,61 @@ import { LOGGED_EVENT_PATTERNS } from './constants';
 export type LogLevel = 'INFO' | 'WARN' | 'ERROR' | 'DEBUG';
 
 export type LogTimestamp = {
-  readonly hh: string;
-  readonly mm: string;
-  readonly ss: string;
+    readonly hh: string;
+    readonly mm: string;
+    readonly ss: string;
 };
 
 export type LoggedMessage = {
-  readonly timestamp: string;
-  readonly thread: string;
-  readonly level: LogLevel;
-  readonly message: string;
+    readonly timestamp: string;
+    readonly thread: string;
+    readonly level: LogLevel;
+    readonly message: string;
 };
 
 export type PresenceEvent<T = 'joined' | 'left'> = {
-  readonly playerName: string;
-  readonly incident: T;
+    readonly playerName: string;
+    readonly incident: T;
 };
 
 export type PlayerJoinedEvent = PresenceEvent<'joined'>;
 export type PlayerLeftEvent = PresenceEvent<'left'>;
 
 export type LifecycleEvent = {
-  readonly incident: string;
+    readonly incident: string;
 };
 
 export type ChatMessageEvent = {
-  readonly playerName: string;
-  readonly messageContent: string;
+    readonly playerName: string;
+    readonly messageContent: string;
 };
 
 export type AnvilSavedEvent = {
-  readonly dimension: string;
+    readonly dimension: string;
 };
 
 export type ProgressEvent = {
-  readonly progress: string;
+    readonly progress: string;
 };
 
 export type LoggedEventData =
-  | PlayerJoinedEvent
-  | PlayerLeftEvent
-  | ChatMessageEvent
-  | AnvilSavedEvent
-  | LifecycleEvent
-  | ProgressEvent
-  | unknown;
+    | PlayerJoinedEvent
+    | PlayerLeftEvent
+    | ChatMessageEvent
+    | AnvilSavedEvent
+    | LifecycleEvent
+    | ProgressEvent
+    | unknown;
 
+/**
+ * Logged event name
+ */
 export type LoggedEventName = keyof typeof LOGGED_EVENT_PATTERNS;
 
+/**
+ * 
+ */
 export type LoggedEvent<T = LoggedEventData | unknown> = LoggedMessage &
-  T & {
-    readonly eventName: LoggedEventName;
-  };
+    T & {
+        readonly eventName: LoggedEventName;
+    };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,12 @@ import { LOGGED_EVENT_PATTERNS, LOGGED_MESSAGE_PATTERN } from './constants';
 import { def, eq, evt, msg } from './operations';
 import { LoggedEvent, LoggedEventName, LoggedMessage, LogLevel } from './types';
 
+/**
+ * Parses a logged message from a logged line.
+ * 
+ * @param rawLog a logged line as string
+ * @returns a log message of type @see LoggedMessage
+ */
 export const createLoggedMessage = (
   rawLog: string
 ): LoggedMessage | undefined => {
@@ -24,6 +30,12 @@ export const createLoggedMessage = (
   };
 };
 
+/**
+ * Parses a logged event from a logged message.
+ * 
+ * @param message The @see LoggedMessage to parse.
+ * @returns An object shaped as @see LoggedEvent
+ */
 export const createLoggedEvent = (
   message?: string | LoggedMessage
 ): LoggedEvent | undefined => {
@@ -46,15 +58,40 @@ export const createLoggedEvent = (
   return undefined;
 };
 
+/**
+ * Transform a path to a stream of @see string or return the provided value if it is already a stream of @see string.
+ * 
+ * @param v A filepath or stream of @see string.
+ * @returns A stream of @see string
+ */
 export const makeLines$ = (v: string | Observable<string>) =>
   typeof v === 'string' ? streamLoggedLines(v) : v;
 
+/**
+ * Transform a path to a stream of @see LoggedMessage or return the provided value if it is already a stream of @see LoggedMessage.
+ * 
+ * @param v a filepath or stream of @see LoggedMessage
+ * @returns A stream of @see LoggedMessage
+ */
 export const makeMessages$ = (v: string | Observable<LoggedMessage>) =>
   typeof v === 'string' ? makeLines$(v) : v.pipe(msg);
 
+/**
+ * Transform a path to a stream of @see Loggedevent or return the provided value if it is already a stream of @see LoggedEvent.
+ * 
+ * @param v a filepath or stream of @see LoggedEvent
+ * @returns A stream of @see LoggedEvent
+ */
 export const makeEvents$ = (v: string | Observable<string | LoggedMessage>) =>
   (typeof v === 'string' ? makeLines$(v) : v).pipe(evt);
 
+/**
+ * Stream logged lines from a logs file.
+ * 
+ * @param source The path of a logs file.
+ * @param options Options to provide to Tail. @see TailOptions
+ * @returns A stream of logged lines (string)
+*/
 export const streamLoggedLines = (
   source: string,
   options?: TailOptions
@@ -66,6 +103,12 @@ export const streamLoggedLines = (
     source$.on('error', (v) => subscriber.error(v));
   });
 
+/**
+ * Stream logged messages.
+ * 
+ * @param source The path of a logs file or an Observable<string>
+ * @returns 
+ */
 export const streamLoggedMessages = (source: string | Observable<string>) =>
   makeLines$(source).pipe(msg, def);
 
@@ -77,12 +120,27 @@ export const streamLoggedEvents = (
     def
   );
 
+/**
+ * Stream logged events matching the provided value for the provided key.
+ * 
+ * @param source The path of a logs file or an Observable<string>
+ * @param key Filter key
+ * @param value Value to match for the provided key
+ * @returns A subset of the source stream
+ */
 export const streamLoggedEventsBy = (
   source: string | Observable<LoggedMessage>,
   key: keyof LoggedEvent,
   value: LoggedEvent[keyof LoggedEvent]
 ) => streamLoggedEvents(source).pipe(eq(key, value));
 
+/**
+ * Stream logged events matching provided eventName.
+ * 
+ * @param source The path of a logs file or an Observable<string>
+ * @param eventName a key of @see LOGGED_EVENT_PATTERNS
+ * @returns A subset of the source stream
+ */
 export const streamLoggedEventsNamed = (
   source: string | Observable<LoggedMessage>,
   eventName: LoggedEventName
