a6d7c874d23ee4b029bbb2aecb571c3f96c5d1f9
LordSequoia <thelordsequoia@gmail.com>
2022-07-06 14:48:14 +0000

    chore: fix

--- src/lib/utils.spec.ts
+++ src/lib/utils.spec.ts
@@ -3,10 +3,10 @@ import test from 'ava';
 import { createLoggedMessage } from './utils';
 
 test('createLoggedMessage', async (t) => {
-    const message = createLoggedMessage(`[00:00:00] [Dummy/INFO]: Hello world`);
+  const message = createLoggedMessage(`[00:00:00] [Dummy/INFO]: Hello world`);
 
-    t.is(message.level, 'INFO');
-    t.is(message.timestamp, '00:00:00');
-    t.is(message.thread, 'Dummy');
-    t.is(message.message, 'Hello world');
+  t.is(message.level, 'INFO');
+  t.is(message.timestamp, '00:00:00');
+  t.is(message.thread, 'Dummy');
+  t.is(message.message, 'Hello world');
 });
