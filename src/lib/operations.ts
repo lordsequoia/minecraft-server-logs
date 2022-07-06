62acfc8e9dbb7c3dc6d26442d36bce31f3452eac
LordSequoia <thelordsequoia@gmail.com>
2022-07-06 16:48:49 +0000

    chore: documentation for operations

--- src/lib/operations.ts
+++ src/lib/operations.ts
@@ -3,19 +3,45 @@ import { filter, map } from 'rxjs';
 import { LoggedEvent, LoggedMessage } from './types';
 import { createLoggedEvent, createLoggedMessage } from './utils';
 
-export const eq = <T>(key: keyof T, value: T[keyof T]) =>
-  filter((v) => v[key] === value);
-export const neq = <T>(key: keyof T, value: T[keyof T]) =>
-  filter((v) => v[key] !== value);
-export const msg = map((v: string | LoggedMessage) =>
-  typeof v !== 'string' ? v : createLoggedMessage(v)
-);
-export const evt = map((v?: LoggedMessage | LoggedEvent) =>
-  typeof v === undefined
-    ? undefined
-    : Object.assign({ eventName: undefined }, v).eventName === undefined
-    ? createLoggedEvent(v)
-    : v
+/**
+ * Creates a stream of T based on an input stream of T matching a value for a specific key.
+ * 
+ * @param key Property name
+ * @param value Value for the property provided in key.
+ * @returns Subset of the input stream emitting only items matching obj[key] === value
+ */
+export const filterEq = <T>(key: keyof T, value: T[keyof T]) =>
+    filter((v) => v[key] === value);
+
+/**
+ * Creates a stream of T based on an input stream of T of items not matching a value for a specific key.
+ * 
+ * @param key Property name
+ * @param value Value for the property provided in key.
+ * @returns Subset of the input stream emitting only items matching obj[key] !== value
+ */
+export const filterNeq = <T>(key: keyof T, value: T[keyof T]) =>
+    filter((v) => v[key] !== value);
+
+/**
+ * Filters a stream to emit only defined values
+ */
+export const filterDef = filter((v?: LoggedEvent) => v !== undefined);
+
+/**
+ * Maps a stream of strings and/or messages to a stream of messages
+ */
+export const mapMessage = map((v: string | LoggedMessage) =>
+    typeof v !== 'string' ? v : createLoggedMessage(v)
 );
 
-export const def = filter((v?: LoggedEvent) => v !== undefined);
+/**
+ * Maps a stream of messages and/or events to a stream of events.
+ */
+export const mapEvent = map((v?: LoggedMessage | LoggedEvent) =>
+    typeof v === undefined
+        ? undefined
+        : Object.assign({ eventName: undefined }, v).eventName === undefined
+            ? createLoggedEvent(v)
+            : v
+);
