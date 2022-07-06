affaede7df3259583f4739521a6769f697bbb6f5
LordSequoia <thelordsequoia@gmail.com>
2022-07-06 16:52:38 +0000

    chore: make single lines

--- src/lib/operations.ts
+++ src/lib/operations.ts
@@ -10,8 +10,7 @@ import { createLoggedEvent, createLoggedMessage } from './utils';
  * @param value Value for the property provided in key.
  * @returns Subset of the input stream emitting only items matching obj[key] === value
  */
-export const filterEq = <T>(key: keyof T, value: T[keyof T]) =>
-  filter((v) => v[key] === value);
+export const filterEq = <T>(key: keyof T, value: T[keyof T]) => filter((v) => v[key] === value);
 
 /**
  * Creates a stream of T based on an input stream of T of items not matching a value for a specific key.
@@ -20,8 +19,7 @@ export const filterEq = <T>(key: keyof T, value: T[keyof T]) =>
  * @param value Value for the property provided in key.
  * @returns Subset of the input stream emitting only items matching obj[key] !== value
  */
-export const filterNeq = <T>(key: keyof T, value: T[keyof T]) =>
-  filter((v) => v[key] !== value);
+export const filterNeq = <T>(key: keyof T, value: T[keyof T]) => filter((v) => v[key] !== value);
 
 /**
  * Filters a stream to emit only defined values
@@ -31,17 +29,15 @@ export const filterDef = filter((v?: LoggedEvent) => v !== undefined);
 /**
  * Maps a stream of strings and/or messages to a stream of messages
  */
-export const mapMessage = map((v: string | LoggedMessage) =>
-  typeof v !== 'string' ? v : createLoggedMessage(v)
-);
+export const mapMessage = map((v: string | LoggedMessage) => typeof v !== 'string' ? v : createLoggedMessage(v));
 
 /**
  * Maps a stream of messages and/or events to a stream of events.
  */
 export const mapEvent = map((v?: LoggedMessage | LoggedEvent) =>
-  typeof v === undefined
-    ? undefined
-    : Object.assign({ eventName: undefined }, v).eventName === undefined
-    ? createLoggedEvent(v)
-    : v
+    typeof v === undefined
+        ? undefined
+        : Object.assign({ eventName: undefined }, v).eventName === undefined
+            ? createLoggedEvent(v)
+            : v
 );
